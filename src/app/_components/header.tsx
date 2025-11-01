afe539a780bc15eaa4831cc10d5cda3668eded52
jlucas <jlucasfchaves@gmail.com>
2025-11-01 12:07:39 -0400

    adicionando paleta de cores automatica no header

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,15 @@
+export function Header () {
+    const word = "TRIUMPH";
+    const repeatCount = 8;
+    
+    return (
+        <header className="flex justify-between h-14 bg-secondary">
+            <div className="justify-start">
+            olá
+            </div>
+            <div className="justify-start">
+            olá
+            </div>
+        </header>
+    )
+}
